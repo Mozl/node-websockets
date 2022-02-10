4cacd98687db335cb27de370f730e48fd58988a7
Mozl <moselhil@gmail.com>
2022-02-10 15:04:22 +0000

    update port

--- server.mjs
+++ server.mjs
@@ -1,8 +1,11 @@
 import { v4 as uuid } from "uuid";
 import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
+import path from "path";
 
-const PORT = 8080;
+const __dirname = path.resolve();
+
+const PORT = 80;
 const INDEX = "/index.html";
 
 console.log("before express.use()");
@@ -15,10 +18,12 @@ const wss = new WebSocketServer({ server });
 
 wss.on("connection", function connection(ws) {
   ws.id = uuid();
+  console.log("client connected");
 
   ws.on("message", function message(info, isBinary) {
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log("sending to clients: ", info.toString("utf8"));
         client.send(info.toString("utf8"), { binary: isBinary });
       }
     });
